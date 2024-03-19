68fddf7270b8b3257c472f5463f8533e758ea5be
zhenyuan wang <zywangdylan@gmail.com>
2024-03-18 22:07:26 -0400

    put the requests into the queue if the refreshToken is refreshing

--- refresh-token-frontend/src/utils/authFetch.js
+++ refresh-token-frontend/src/utils/authFetch.js
@@ -1,5 +1,19 @@
 import { getAuthToken, setAuthToken } from './localStorage';
+
 const API_BASE_URL = 'http://localhost:3000'; // Adjust according to your server's address
+let isRefreshing = false;
+let requests = [];
+
+const processQueue = (error, token = null) => {
+  requests.forEach(prom => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token);
+    }
+  });
+  requests = [];
+}
 
 async function refreshToken() {
   // Assuming the refresh token is stored in localStorage
@@ -22,28 +36,64 @@ async function refreshToken() {
     return data.accessToken;
   } catch (error) {
     console.error('Refresh Token Error:', error);
+    // TODO: Redirect to login page
     throw error;
+  } finally {
+    isRefreshing = false;
   }
 }
 
 async function authFetch(url, options) {
   let res = await fetch(url, options);
+  let accessToken = getAuthToken('accessToken');
 
-  // Check if the token is expired
-  if (response.status === 409) {
-    try {
-      const newAccessToken = await refreshToken();
-      // Update the Authorization header with the new token
-      options.headers['Authorization'] = `Bearer ${newAccessToken}`;
-      // Retry the fetch with the new token
-      response = await fetch(url, options);
-    } catch (refreshError) {
-      console.error('Error refreshing token:', refreshError);
-      throw refreshError;
+  const executeFetch = async ()  => {
+    const res = await fetch(url, {
+      ...options,
+      'Authorization': `Bearer ${accessToken}`,
+    });
+    return res;
+  }
+
+  const response = await executeFetch();
+  const data = await response.json();
+
+  if (response.ok) {
+    return response;
+  } else if (response.status === 409) {
+    if (!isRefreshing) {
+      isRefreshing = true;
+      refreshToken.then(newToken => {
+        processQueue(null, newToken)
+      }).catch(err => {
+        processQueue(error, null)
+        return Promise.reject(error)
+      })
     }
+
+    return new Promise((resolve, reject) => {
+      failedQueue.push({
+        resolve: () => resolve(authFetch(url, options)),
+        reject: () => reject(data),
+      });
+    });
+  } else {
+    // Directly reject for all other errors
+    return Promise.reject(data);
   }
 
   return response;
 }
 
 export default authFetch;
+
+// authFetch(API_URL + '/endpoint', {
+//   method: 'GET',
+//   // Additional options...
+// })
+// .then(data => {
+//   // Handle successful response
+// })
+// .catch(error => {
+//   // Handle error
+// });
